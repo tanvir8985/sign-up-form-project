3224c6b2d3ed7aafa4ee96c2fcdc90f83d7e9892
tanvir8985 <54028698+tanvir8985@users.noreply.github.com>
2019-12-29 20:22:05 -0500

    prettier format

--- assets/js/signup.js
+++ assets/js/signup.js
@@ -1,51 +1,45 @@
-
 $("#submitButton").click(function() {
-    var errorMessage = ""
-    var fieldMissing = ""
-//checking for missing values
-    if ($("#email").val() == "") {
-        fieldMissing += " EMAIL"
-    }
-    if ($("#phone").val() == "") {
-        fieldMissing += ", PHONE NUMBER"
-    }
-    if ($("#password").val() == "") {
-        fieldMissing += ", PASSWORD"
-    }
-    if ($("#confirmPassword").val() == "") {
-        fieldMissing += ", CONFIRM PASSWORD"
-    }
-    if (fieldMissing != "") {
-        errorMessage += "The following fields are missing: " + fieldMissing;
-    }
+  var errorMessage = "";
+  var fieldMissing = "";
+  //checking for missing values
+  if ($("#email").val() == "") {
+    fieldMissing += " EMAIL";
+  }
+  if ($("#phone").val() == "") {
+    fieldMissing += ", PHONE NUMBER";
+  }
+  if ($("#password").val() == "") {
+    fieldMissing += ", PASSWORD";
+  }
+  if ($("#confirmPassword").val() == "") {
+    fieldMissing += ", CONFIRM PASSWORD";
+  }
+  if (fieldMissing != "") {
+    errorMessage += "The following fields are missing: " + fieldMissing;
+  }
 
-//this is email validation
-    function isEmail(email) {
-  var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
-  return regex.test(email);
-}
+  //this is email validation
+  function isEmail(email) {
+    var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+    return regex.test(email);
+  }
 
-if (isEmail($("#email").val()) == false ) {
-    
-    errorMessage += "//Your email address is not valid"
-}
-// this is number validation
-if ($.isNumeric($("#phone").val()) == false ) {
-    errorMessage += ", your phone number is not valid"
-}
-// this is password match fuction
-if ($("#password").val() != $("#confirmPassword").val()) {
-    errorMessage += ", your passwords don't match"
-}
-//showing error on browser
-if (errorMessage != "") {
+  if (isEmail($("#email").val()) == false) {
+    errorMessage += "//Your email address is not valid";
+  }
+  // this is number validation
+  if ($.isNumeric($("#phone").val()) == false) {
+    errorMessage += ", your phone number is not valid";
+  }
+  // this is password match fuction
+  if ($("#password").val() != $("#confirmPassword").val()) {
+    errorMessage += ", your passwords don't match";
+  }
+  //showing error on browser
+  if (errorMessage != "") {
     $("#errorMessage").text(errorMessage);
-}
-else {
+  } else {
     $("#signUp").text("You signed up! YAY!!!");
     $("#errorMessage").hide();
-
-    }
-
-
+  }
 });
